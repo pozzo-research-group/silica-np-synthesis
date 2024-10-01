dcd1b28ecd600416f8b8c31ab37eb4db7dbc58e7
Brenden <bgpelkie@gmail.com>
2024-10-01 15:32:14 -0700

    debug new synthesis utils

--- stober_synthesis_utils.py
+++ stober_synthesis_utils.py
@@ -1,157 +1,155 @@
-%load_ext autoreload
-%autoreload 2
-
 import requests
 import pandas as pd
 from science_jubilee import Machine as science_jubilee
 from science_jubilee.tools import HTTPSyringe as syringe
-from science_jubilee.tools import pipette
+from science_jubilee.tools import Pipette
 import time
+import numpy as np
+import logging
 
-url = 'http://192.168.1.5:5000'
-volume = {'volume': 5}
-r = requests.post(url + '/get_status', json = {'name': '1cc_2'})
-r.content
+logger = logging.getLogger(__name__)
 
-jubilee = Jub.Machine(address = '192.168.1.2', simulated = False)
-# jubilee.home_all()
-deck = jubilee.load_deck('lab_automation_deck_AFL_bolton.json')
+def _update_current_stock(stocks, stock_volumes, transfer_volume):
+    """
+    Find the next appropriate stock vial with enough stock for the transfer
+    """
+    names = {stock.name:stock for stock in stocks}
 
+    for name in names:
+        if stock_volumes[name] > transfer_volume:
+            return names[name]
+    
+    raise AssertionError(f'Ran out of ethanol stocks - no vial contains enough for {transfer_volume} uL transfer. Current volumes: {stock_volumes}')
 
-## Load labware
-samples = jubilee.load_labware('septavialrev1_44_holder_2000ul.json, 2')
-samples.manual_offset([(19.8,178.1),(132.5,177.6),(132.5,106.6)])
+def count_stock_vials(sample_table, volume_per_vial):
 
-stocks = jubilee.load_labware('20mlscintillation_12_wellplate_18000ul.json', 4)
-stocks.manual_offset([(31.3,268.1),(117.6, 267),(117.9, 210.9)])
+    teos_count = int(np.ceil(sample_table['teos_volume'].sum()/volume_per_vial))
+    ammonia_count = int(np.ceil(sample_table['ammonia_volume'].sum()/volume_per_vial))
+    water_count = int(np.ceil(sample_table['water_volume'].sum()/volume_per_vial))
+    ethanol_count = int(np.ceil(sample_table['ethanol_volume'].sum()/volume_per_vial))
 
-tiprack = jubilee.load_labware('opentron_96_tiprack_300ul.json', 0)
-tiprack.manual_offset([(28,77.3),(127.1, 76.8),(127.8, 14.5)])
-
-trash = jubilee.load_labware('agilent_1_reservoir_290ml.json', 1)
-
-## Load tools
-P300 = Pipette.Pipette.from_config(0, 'Pipette', 'P300_config.json')
-jubilee.load_tool(P300)
-P300.add_tiprack(tiprack)
-P300.trash = trash[0]
-# xxx.from_config(#, etc), the # relate to the tool number in the main page of science jubilee
-syringe_10 = syringe.HTTPSyringe.from_config(1, "src/science_jubilee/tools/configs/10cc_syringe.json")
-syringe_1_1 = syringe.HTTPSyringe.from_config(2, "src/science_jubilee/tools/configs/1cc_1_syringe.json")
-syringe_1_2 = syringe.HTTPSyringe.from_config(3, "src/science_jubilee/tools/configs/1cc_2_syringe.json")
-syringe_1_3 = syringe.HTTPSyringe.from_config(4, "src/science_jubilee/tools/configs/1cc_3_syringe.json")
-
-jubilee.load_tool(syringe_10)
-jubilee.load_tool(syringe_1_1)
-jubilee.load_tool(syringe_1_2)
-jubilee.load_tool(syringe_1_3)
-
-mix_syringe = syringe_10
-water_syringe = syringe_1_1
-ammonia_syringe = syringe_1_2
-teos_syringe = syringe_1_3
-
-df = pd.read_csv('xxx.csv')
-# assign and name the list with the heading 'ethanol ratio' in df as ethanol_ratio
-ethanol_vol = list(df['ethanol_volume'])
-ammonia_vol = list(df['ammonia_volume'])
-water_vol = list(df['water_volume'])
-TEOS_vol = list(df['TEOS_volume'])
-mix_vol = 1990
-wells = list(df['well'])
-
-# define location of the stocks
-ethanol_stock = stocks[0]
-ammonia_stock = stocks[1]
-water_stock = stocks[2]
-TEOS_stock = stocks[3]
-wash_stock_1 = stocks[4]
-wash_stock_2 = stocks[5]
-
+    return {'teos_count':teos_count, 'ammonia_count':ammonia_count, 'water_count':water_count, 'ethanol_count':ethanol_count}
 
 
 # this is for etoh
-def add_etoh(volumes, stocks, wells):
+def add_etoh(jubilee, P300, sample_table, location_lookup, ethanol_stocks, max_pipette_volume = 290, stocks_usable_volume = 17000):
     jubilee.pickup_tool(P300)
     P300.pickup_tip()
-    # The maximum volume that a P300 pipette can handle is 300 μL
-    max_pipette_volume = 300
-    for volume in volumes:
-        for well_name in wells:
-            dispense_loc = samples[well_name]
-    # Check if the volume is larger than 300
-            if volume > max_pipette_volume:
-        # Calculate how many parts the volume should be divided into
-                parts = (volume // max_pipette_volume) + (1 if volume % max_pipette_volume > 0 else 0)
-        
-        # Calculate the volume per part
-                divided_volume = volume / parts
-        
-        #print(f"Dividing {volume} μL into {parts} parts, each {divided_volume:.2f} μL.")
-        
-        # Simulate aspirating each divided volume using the P300 pipette
-                for i in range(parts):
-                    P300.aspirate(divided_volume, stocks)
-                    jubilee.move(dz = 60)
-                    P300.dispense(divided_volume, dispense_loc.bottom(+15))
-                    jubilee.move(dz = 60)
-                    time.sleep(2)
-            #print(f"Aspirating {divided_volume:.2f} μL with P300 pipette (Part {i+1}/{parts})")
-             else:
-        # If the volume is 300 or less, just aspirate it directly
-                P300.aspirate(volume, stocks)
-                jubilee.move(dz = 60)
-                P300.dispense(volume, dispense_loc.bottom(+15))
-                jubilee.move(dz = 60)
-                time.sleep(2)
-        #print(f"Aspirating {volume} μL with P300 pipette.")
-    P300.return_tip()
+    # The maximum volume that a P300 pipette can handle is 300 μL\
+
+    stock_volumes = {stock.name:stocks_usable_volume for stock in ethanol_stocks}
+
+    for i, row in sample_table.iterrows():
+        uuid = row['uuid']
+        etoh_vol = row['ethanol_volume']
+
+        dispense_loc = location_lookup[uuid]
+
+        ### Transfer split calculation
+        n_full_transfers = int(np.floor(etoh_vol / max_pipette_volume))
+        final_etoh_vol = etoh_vol - n_full_transfers*max_pipette_volume
+        #########
+
+        # etoh transfer execution
+        for k in range(n_full_transfers):
+            current_stock = _update_current_stock(ethanol_stocks, stock_volumes, max_pipette_volume)
+            P300.aspirate(max_pipette_volume, current_stock)
+            P300.dispense(max_pipette_volume, dispense_loc.bottom(+22), s = 1000)
+            time.sleep(3)
+            stock_volumes[current_stock.name] -= max_pipette_volume
+            logger.info(f'Dispensed {max_pipette_volume} uL ethanol into {dispense_loc}')
+        # final transfer to hit vol
+        current_stock = _update_current_stock(ethanol_stocks, stock_volumes, final_etoh_vol)
+        P300.aspirate(final_etoh_vol, current_stock)
+        P300.dispense(final_etoh_vol, dispense_loc.bottom(+22))
+        logger.info(f'Dispensed {final_etoh_vol} uL ethanol into {dispense_loc}')
+        time.sleep(3)
+        stock_volumes[current_stock.name] -= final_etoh_vol
+
+        logger.info(f'Ethanol dispense complete: {etoh_vol} uL Ethanol dispensed into {dispense_loc}')
+
+
+    P300.drop_tip()
     jubilee.park_tool()
+
+def _refill_syringe(syringe, stocks, stock_volumes):
+    #1. get current stock to use
+    aspirate_vol = syringe.capacity - syringe.remaining_volume - 1 
+    current_stock = _update_current_stock(stocks, stock_volumes, aspirate_vol)
+    #2. aspirate from stock
+
+    logger.info(f'Refilling {syringe.name} with {aspirate_vol} uL from {current_stock}')
+    syringe.aspirate(aspirate_vol, current_stock, s = 10)
+    #3. update remaining volumes
+    stock_volumes[current_stock.name] -= aspirate_vol
+
+    return stock_volumes
 
 # for ammonia, water and TEOS
-def add_reactants(syringe, volumes, stocks, wash_stock, well):
-    jubilee.pickup_tool(syringe)
+def add_reactants_batch(jubilee, reactant_syringe, mix_syringe, sample_table, location_lookup, reactant_name, stocks, stocks_usable_volume = 17000, mix_after = None ):
+    
+    if mix_after is not None:
+        assert isinstance(mix_after, tuple), 'mix after must be a tuple containing mix_volume, n_mix, wash stocks'
+        assert len(mix_after) == 3, 'mix after must be a tuple containing mix_volume, n_mix, wash stocks'
 
-    for volume in volumes:
-        for well_name in well:
-            dispense_location = samples[well_name]
+        mix_vol = mix_after[0]
+        n_mix = mix_after[1]
+        wash_stocks = mix_after[2]
+    
+    jubilee.pickup_tool(reactant_syringe)
 
-            if volume < syringe.remaining_volume:
-                syringe.dispense(volume, dispense_location)
-                time.sleep(3)
-            else:
-                jubilee.move(dz = 60)
-                jubilee.move_to(wash_stock)
-                jubilee.move(dz = -60)
-                jubilee.move(dz = 60)
-                time.sleep(15)
-                aspirate_vol = 995-syringe.remaining_volume
+    stock_volumes = {stock.name:stocks_usable_volume for stock in stocks}
+
+
+    for i, row in sample_table.iterrows():
+
+        uuid = row['uuid']
+        dispense_volume = row[reactant_name]
+
+        dispense_location = location_lookup[uuid]
+
+        # make sure syringe has enough volume
+        if dispense_volume > reactant_syringe.remaining_volume:
+            stock_volumes = _refill_syringe(reactant_syringe, stocks, stock_volumes)
+
+        reactant_syringe.dispense(dispense_volume, dispense_location, s = 20)
+        time.sleep(3)
+        logger.info(f'Dispensed {dispense_volume} uL of {reactant_name} into {dispense_location}')
+
+        if mix_after is not None:
+            jubilee.park_tool()
+            jubilee.pickup_tool(mix_syringe)
             
-                syringe.aspirate(aspirate_vol, stocks, s = 10)
-                syringe.dispense(volume, dispense_location)
-                time.sleep(3)
+            mix_syringe.mix(mix_vol, n_mix, dispense_location.bottom(+5), s_aspirate = 2000, s_dispense = 500)
+            logger.info(f'Mixed well {dispense_location} {n_mix} times, {mix_vol} uL per cycle')
+
+            for stock in wash_stocks:
+                mix_syringe.mix(mix_vol, 5, stock.bottom(+10), t_hold = 3, s_aspirate = 2000, s_dispense = 500)
+
+            logger.info(f'Washed mix syringe in wash solutions {wash_stocks}')
+
+            jubilee.park_tool()
+            jubilee.pickup_tool(reactant_syringe)
+
+
     jubilee.park_tool()
 
-def first_mix(mix_vol, well, wash_stock_1, wash_stock_2):
+
+def first_mix(jubilee, mix_syringe, mix_vol, location_lookup, wash_stocks, n_mix):
     jubilee.pickup_tool(mix_syringe)
     
-    for well_name in well:
-        mix_loc = samples[well_name]
-        mix_syringe.aspirate(5, mix_loc.bottom(+5))
-        mix_syringe.mix(mix_vol, 5, mix_loc, t_hold = 3)
-        
-        mix_syringe.aspirate(5, wash_stock_1)
-    
-        for k in range(2):
-            wash_loc = stocks[k+1]
-            mix_syringe.mix(mix_vol, 3, wash_loc, t_hold = 3)
+    for name, well in location_lookup.items():
+        mix_syringe.mix(mix_vol, n_mix, well.bottom(+5), s_aspirate = 2000, s_dispense = 500)
+        logger.info(f'Mixed well {well} {n_mix} times, {mix_vol} uL per cycle')
+
+        for stock in wash_stocks:
+            mix_syringe.mix(mix_vol, 5, stock.bottom(+10), t_hold = 3, s_aspirate = 2000, s_dispense = 500)
+
+        logger.info(f'Washed mix syringe in wash solutions {wash_stocks}')
+
     jubilee.park_tool()
 
 
 
-        
-add_etoh(ethanol_vol, ethanol_stock, wells)
-#add_reactants(syringe, volumes, stocks, well)
-add_reactants(ammonia_syringe, ammonia_vol, ammonia_stock, wash_stock_1, wells)
-add_reactants(water_syringe, water_vol, water_stock, wash_stock_1, wells)
+
 
